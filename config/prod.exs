9d47ce1bcf2da53a375b7df1cc37537f3aa045ab
MichaelDimmitt <michaelgdimmitt@gmail.com>
2017-04-11 14:31:23 -0400

    update prod configuration

--- config/prod.exs
+++ config/prod.exs
@@ -58,4 +58,13 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
+# Configure secret_key_base for cookie-based session storage
+  config :unf_swuber, UnfSwuber.Endpoint,
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
+
+# Configure your database
+  config :unf_swuber, UnfSwuber.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  size: 20 # The amount of database connections in the pool
